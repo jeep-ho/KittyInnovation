0842b2a63b9e67b14470ecf4afb2ddb55fd421e2
jeep-ho <jeep-ho@hotmail.com>
2017-08-03 18:18:44 +0800

    Add Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<<template>
+<div class="Login">
+{{msg}}    
+</div>
+</template>
+<<script lang="ts">
+export default {
+  name:'Login',
+  data(){
+      return new {msg:'Login'}
+  }
+}
+</script>
+<<style scoped>
+  div {
+      font-size:14px;
+  }
+</style>
+
+
